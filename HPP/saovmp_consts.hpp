094791142e6064c7954ff30e950a9fd880d0bf6c
safrazRampersaud <safraz@wayne.edu>
2016-03-22 21:28:33 -0400

    Added files via upload

---
+++ HPP/saovmp_consts.hpp
@@ -0,0 +1,16 @@
+#ifndef SAOVMP_CONSTS_HPP
+#define SAOVMP_CONSTS_HPP
+
+namespace saovmp_consts
+	{
+		const int numOfAps 			=	5;
+		const int numOfOps			=	1;
+		const int numOfAPVers		=	8;
+		const int numOfOPVers		=	10;
+		const int slotSizeOS		=	16;
+		const int slotSizeAP		=	16;
+		const int machTypePerCell 	=	15;
+		const int NUM_STRATS		=	10;
+		const int SERVER_TYPES		=	8;
+	};
+#endif
